19e80c21aa48ac6e9bd7446e8df51cf755f8fe6d
Chimo <stephberube@gmail.com>
2012-07-07 02:06:44 -0400

    Initial commit.

---
+++ BookmarkList/BookmarkListPlugin.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * StatusNet - the distributed open-source microblogging tool
+ * Copyright (C) 2010, StatusNet, Inc.
+ *
+ * A plugin to enable social-bookmarking functionality
+ *
+ * PHP version 5
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  SocialBookmark
+ * @package   StatusNet
+ * @author    Evan Prodromou <evan@status.net>
+ * @copyright 2010 StatusNet, Inc.
+ * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html AGPL 3.0
+ * @link      http://status.net/
+ */
+
+if (!defined('STATUSNET')) {
+    exit(1);
+}
+
+/**
+ * Bookmark plugin main class
+ *
+ * @category  Bookmark
+ * @package   StatusNet
+ * @author    Brion Vibber <brionv@status.net>
+ * @author    Evan Prodromou <evan@status.net>
+ * @copyright 2010 StatusNet, Inc.
+ * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html AGPL 3.0
+ * @link      http://status.net/
+ */
+class BookmarkListPlugin extends Plugin
+{
+    const VERSION         = '0.1';
+
+    /**
+     * Show the CSS necessary for this plugin
+     *
+     * @param Action $action the action being run
+     *
+     * @return boolean hook value
+     */
+    function onEndShowStyles($action)
+    {
+        $action->cssLink($this->path('bookmark.css'));
+        return true;
+    }
+
+    function onEndShowScripts($action)
+    {
+        $action->script($this->path('js/bookmark.js'));
+        return true;
+    }
+    /**
+     * Load related modules when needed
+     *
+     * @param string $cls Name of the class to be loaded
+     *
+     * @return boolean hook value; true means continue processing, false means stop.
+     */
+    function onAutoload($cls)
+    {
+        $dir = dirname(__FILE__);
+
+        switch ($cls)
+        {
+        case 'BookmarksAction':
+            include_once $dir . '/' . strtolower(mb_substr($cls, 0, -6)) . '.php';
+            return false;
+        default:
+            return true;
+        }
+    }
+
+    /**
+     * Map URLs to actions
+     *
+     * @param Net_URL_Mapper $m path-to-action mapper
+     *
+     * @return boolean hook value; true means continue processing, false means stop.
+     */
+    function onRouterInitialized($m)
+    {
+        $m->connect('bookmarks',
+                    array('action' => 'bookmarks'));
+
+        return true;
+    }
+
+    /**
+     * Plugin version data
+     *
+     * @param array &$versions array of version data
+     *
+     * @return value
+     */
+    function onPluginVersion(&$versions)
+    {
+        $versions[] = array('name' => 'BookmarkList',
+                            'version' => self::VERSION,
+                            'author' => 'Stephane Berube',
+                            'homepage' => 'http://github.com/chimo/sn-bookmark-list',
+                            'description' =>
+                            // TRANS: Plugin description.
+                            _m('Simple extension for displaying bookmarks.'));
+        return true;
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ sn-bookmark-list
 ================
 
 Tiny plugin to display your StatusNet Bookmarks
+
+## Instructions
+
+Drop the files in your `/plugins/` folder  
+Tell `/config.php` to use it with: `addPlugin('BookmarkList');`
+Visit `/bookmarks` on your SN instance to view your bookmarks.
